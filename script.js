fa1b399990016d16c15f509239b799b6d9ab6eef
Shubh-devv <shubhamd1422@gmail.com>
2023-12-15 01:04:56 +0530

    commit

---
+++ script.js
@@ -0,0 +1,205 @@
+function loco(){
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+el: document.querySelector("#main"),
+smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+scrollTop(value) {
+  return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+}, // we don't have to define a scrollLeft because we're only scrolling vertically.
+getBoundingClientRect() {
+  return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+},
+// LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+}
+loco()
+
+var clutter = "";
+
+document.querySelector("#page2>h1").textContent.split("").forEach(function(dets){
+    clutter += `<span>${dets}</span>`
+
+    document.querySelector("#page2>h1").innerHTML = clutter;
+})
+
+
+gsap.to("#page2>h1>span",{
+    scrollTrigger:{
+        trigger:`#page2>h1>span`,
+        start:`top bottom`,
+        end:`bottom top`,
+        scroller:`#main`,
+        scrub:0.5,
+        // markers:true,
+    },
+    stagger:0.2,
+    color:`#fff`
+})
+
+
+function canvas(){
+  const canvas = document.querySelector("#page3>canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+window.addEventListener("resize", function () {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+render();
+});
+
+function files(index) {
+var data = `
+./Images/frames00007.png
+./Images/frames00010.png
+./Images/frames00013.png
+./Images/frames00016.png
+./Images/frames00019.png
+./Images/frames00022.png
+./Images/frames00025.png
+./Images/frames00028.png
+./Images/frames00031.png
+./Images/frames00034.png
+./Images/frames00037.png
+./Images/frames00040.png
+./Images/frames00043.png
+./Images/frames00046.png
+./Images/frames00049.png
+./Images/frames00052.png
+./Images/frames00055.png
+./Images/frames00058.png
+./Images/frames00061.png
+./Images/frames00064.png
+./Images/frames00067.png
+./Images/frames00070.png
+./Images/frames00073.png
+./Images/frames00076.png
+./Images/frames00079.png
+./Images/frames00082.png
+./Images/frames00085.png
+./Images/frames00088.png
+./Images/frames00091.png
+./Images/frames00094.png
+./Images/frames00097.png
+./Images/frames00100.png
+./Images/frames00103.png
+./Images/frames00106.png
+./Images/frames00109.png
+./Images/frames00112.png
+./Images/frames00115.png
+./Images/frames00118.png
+./Images/frames00121.png
+./Images/frames00124.png
+./Images/frames00127.png
+./Images/frames00130.png
+./Images/frames00133.png
+./Images/frames00136.png
+./Images/frames00139.png
+./Images/frames00142.png
+./Images/frames00145.png
+./Images/frames00148.png
+./Images/frames00151.png
+./Images/frames00154.png
+./Images/frames00157.png
+./Images/frames00160.png
+./Images/frames00163.png
+./Images/frames00166.png
+./Images/frames00169.png
+./Images/frames00172.png
+./Images/frames00175.png
+./Images/frames00178.png
+./Images/frames00181.png
+./Images/frames00184.png
+./Images/frames00187.png
+./Images/frames00190.png
+./Images/frames00193.png
+./Images/frames00196.png
+./Images/frames00199.png
+./Images/frames00202.png
+`;
+return data.split("\n")[index];
+}
+
+const frameCount = 67;
+
+const images = [];
+const imageSeq = {
+frame: 1,
+};
+
+for (let i = 0; i < frameCount; i++) {
+const img = new Image();
+img.src = files(i);
+images.push(img);
+}
+
+gsap.to(imageSeq, {
+frame: frameCount - 1,
+snap: "frame",
+ease: `none`,
+scrollTrigger: {
+  scrub: .5,
+  trigger: `#page3`,
+  start: `top top`,
+  end: `250% top`,
+  scroller: `#main`,
+},
+onUpdate: render,
+});
+
+images[1].onload = render;
+
+function render() {
+scaleImage(images[imageSeq.frame], context);
+}
+
+function scaleImage(img, ctx) {
+var canvas = ctx.canvas;
+var hRatio = canvas.width / img.width;
+var vRatio = canvas.height / img.height;
+var ratio = Math.max(hRatio, vRatio);
+var centerShift_x = (canvas.width - img.width * ratio) / 2;
+var centerShift_y = (canvas.height - img.height * ratio) / 2;
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.drawImage(
+  img,
+  0,
+  0,
+  img.width,
+  img.height,
+  centerShift_x,
+  centerShift_y,
+  img.width * ratio,
+  img.height * ratio
+);
+}
+ScrollTrigger.create({
+
+trigger: "#page3",
+pin: true,
+scroller: `#main`,
+start: `top top`,
+end: `250% top`,
+});
+}
+canvas()
+
